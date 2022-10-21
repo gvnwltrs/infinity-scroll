2fdf420c2f2266b276c909e96530040f04b09bdf
Gavin <waltersg298@gmail.com>
2022-10-21 11:29:44 -0600

    Implemented loader and a few images. Also created repsonsive layout.

---
+++ tests/script.test.js
@@ -0,0 +1,12 @@
+import { executionAsyncId } from 'async_hooks';
+import {describe, it, expect, vi } from 'vitest'; 
+
+import {test} from "../index.html";
+
+describe("Infinity-scroll Tests", () => {
+
+    it("should do something here", ()=> {
+        expect(true).toEqual(true);
+    });
+
+});
