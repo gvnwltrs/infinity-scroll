e0cf3dbee00e86566ed679910b0aa840def12d58
Gavin <waltersg298@gmail.com>
2022-10-22 14:14:15 -0600

    finished loader and infinite scroll procedure

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const imageContainer = document.getElementById('image-container'); 
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0; 
+let totalImages = 0; 
 let photosArray = []; 
 
 // unsplash api
-const count = 10;
+const count = 30;
 const apiKey = 'y4Q_VuqSkl7ShYzHFATbTdp4RxEzdZlThPy_HZNEGG4';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    console.log(imagesLoaded); 
+    if (imagesLoaded === totalImages) {
+        ready = true; 
+        loader.hidden = true;
+        console.log('ready =', ready); 
+    }
+}
 
 // helper function to set attributes on DOM elements 
 function setAttributes(element, attributes) {
@@ -17,6 +31,9 @@ function setAttributes(element, attributes) {
 
 // create elements for links & photos, add to DOM
 function displayPhotos() {
+    imagesLoaded = 0; 
+    totalImages = photosArray.length;
+    console.log('total images', totalImages); 
     // run function for each object in photosArray
     photosArray.forEach((photo) => {
         // create <a> to link to unsplash
@@ -32,16 +49,17 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description,
         });
+        // event listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded); 
         // put <img> inside <a>, then put both inside imageContainer element
         item.appendChild(img); 
         imageContainer.appendChild(item); 
     }); 
-
-    return true;
 }
 
 // get photos from unsplash api
 async function getPhotos() {
+
     try {
         const response = await fetch(apiUrl);
         photosArray = await response.json();
@@ -51,6 +69,14 @@ async function getPhotos() {
     }
 }
 
+// check to see if scrolling near bottom of page, load more photos 
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+}); 
+
 // test 
 function test() {
     return 'hello';
@@ -59,4 +85,5 @@ function test() {
 // on load 
 getPhotos();
 
-export {test, apiUrl, count, apiKey, getPhotos, photosArray}; 
+export {test, apiUrl, count, apiKey, getPhotos, photosArray, displayPhotos}; 
+// module.exports = {test, apiUrl, count, apiKey, getPhotos, photosArray, displayPhotos}; 
--- test/script.test.js
+++ test/script.test.js
@@ -1,23 +1,25 @@
-import { executionAsyncId } from 'async_hooks';
-import {describe, it, expect, vi } from 'vitest'; 
-
 //create virtual DOM
+import {beforeEach, describe, it, expect, vi } from 'vitest'; 
 import fs from 'fs';
 import path from 'path';
 import { Window } from 'happy-dom'
-
 const htmlDocPath = path.join(process.cwd(), 'index.html');
 const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
-
-
 const window = new Window();
 const document = window.document;
 document.write(htmlDocumentContent);
 vi.stubGlobal('document', document);
 
+import { executionAsyncId } from 'async_hooks';
 import {test, apiUrl, count, apiKey, getPhotos, displayPhotos} from '../script.js';
 
+// const _script = require("../script.js");
+// const chai = chai.assert;
+
 describe("Infinity-scroll Tests", () => {
+    beforeEach( async () => {
+      
+    });
 
     it("should return hello after test function call", () => {
         let output = test();
@@ -40,8 +42,17 @@ describe("Infinity-scroll Tests", () => {
         expect(getPhotos).toBeDefined();
     }); 
 
-    // it("should have something to display all the photos", () => {
-    //     expect(displayPhotos).toBeDefined(); 
-    // });
+    it("should have something to display all the photos", () => {
+        expect(displayPhotos).toBeDefined(); 
+    });
 
+    it("should have images loaded", () => {
+        const imageContainer = document.getElementById('image-container');
+        const photos = imageContainer.childElementCount;
+        console.log(photos); 
+
+        expect(photos).not.toBeNull();
+    });
 });
+
+// mocha.run();
