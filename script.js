f27181e3273b762a7caf8111991739ace840a6db
Gavin <waltersg298@gmail.com>
2022-10-21 18:46:58 -0600

    now actually adding images from unsplash web source

--- script.js
+++ script.js
@@ -1,6 +1,62 @@
+const imageContainer = document.getElementById('image-container'); 
+const loader = document.getElementById('loader');
 
-function test() {
-    console.log('hello');
+let photosArray = []; 
+
+// unsplash api
+const count = 10;
+const apiKey = 'y4Q_VuqSkl7ShYzHFATbTdp4RxEzdZlThPy_HZNEGG4';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// helper function to set attributes on DOM elements 
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]); 
+    }
 }
 
-export {test}; 
+// create elements for links & photos, add to DOM
+function displayPhotos() {
+    // run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // create <a> to link to unsplash
+        const item = document.createElement('a'); 
+        // create <img> for photo 
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        }); 
+        const img = document.createElement('img'); 
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+        // put <img> inside <a>, then put both inside imageContainer element
+        item.appendChild(img); 
+        imageContainer.appendChild(item); 
+    }); 
+
+    return true;
+}
+
+// get photos from unsplash api
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos(); 
+    } catch (error) {
+
+    }
+}
+
+// test 
+function test() {
+    return 'hello';
+}
+
+// on load 
+getPhotos();
+
+export {test, apiUrl, count, apiKey, getPhotos, photosArray}; 
--- tests/script.test.js
+++ tests/script.test.js
@@ -1,12 +1,47 @@
 import { executionAsyncId } from 'async_hooks';
 import {describe, it, expect, vi } from 'vitest'; 
 
-import {test} from "../index.html";
+//create virtual DOM
+import fs from 'fs';
+import path from 'path';
+import { Window } from 'happy-dom'
+
+const htmlDocPath = path.join(process.cwd(), 'index.html');
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
+
+
+const window = new Window();
+const document = window.document;
+document.write(htmlDocumentContent);
+vi.stubGlobal('document', document);
+
+import {test, apiUrl, count, apiKey, getPhotos, displayPhotos} from '../script.js';
 
 describe("Infinity-scroll Tests", () => {
 
-    it("should do something here", ()=> {
-        expect(true).toEqual(true);
+    it("should return hello after test function call", () => {
+        let output = test();
+        expect(output).toEqual('hello');
     });
+
+    it("should have api url content", () => {
+        expect(apiUrl).toBeDefined();
+    });
+
+    it("should have a count set for 10 photos", () => {
+        expect(count).toBe(10); 
+    });
+
+    it("should have a valid unsplash api key set", () => {
+        expect(apiKey).not.toEqual(''); 
+    });
+
+    it("should have function to get random photos", () => {
+        expect(getPhotos).toBeDefined();
+    }); 
+
+    // it("should have something to display all the photos", () => {
+    //     expect(displayPhotos).toBeDefined(); 
+    // });
 
 });
